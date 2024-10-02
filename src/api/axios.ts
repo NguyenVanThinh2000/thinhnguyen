81002c580ec22af8209f7d340d2e7bf5bd15d96d
thinhnguyen <nguyenvanthinh2701@gmail.com>
2024-10-02 23:45:52 +0700

    first commit

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import queryString from 'query-string'
 
 const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://thinhnguyen-be.vercel.app/api',
   headers: {},
   paramsSerializer: (params) =>
     queryString.stringify(params, {

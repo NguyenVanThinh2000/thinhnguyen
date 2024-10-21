15b18e6cd8f70bb1babcfe54d635dfbba4f58882
thinhnguyen <nguyenvanthinh2701@gmail.com>
2024-10-21 22:30:09 +0700

    update

--- src/components/guest-management/tabe-actions/index.tsx
+++ src/components/guest-management/tabe-actions/index.tsx
@@ -1,3 +1,9 @@
+import { Link } from 'react-router-dom'
+
+import { SquareArrowOutUpRight } from 'lucide-react'
+
+import { Button } from '@/components/ui'
+import { ENV } from '@/config'
 import { TGuestResponse } from '@/types'
 
 import DeleteGuest from '../delete-guest'
@@ -9,6 +15,11 @@ interface TableActionsProps {
 const TableActions = ({ guest }: TableActionsProps) => {
   return (
     <div className="flex min-w-20 space-x-2">
+      <Link target="_blank" to={`${ENV.INVITATION_URL}/?code=${guest.id}`}>
+        <Button className="size-8" size="icon" variant="outline">
+          <SquareArrowOutUpRight size={16} />
+        </Button>
+      </Link>
       <EditGuest guest={guest} />
       <DeleteGuest id={guest.id} />
     </div>
